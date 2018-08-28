76cb03b735e5ca505bc7f2db0b7d74ca6ca77787
Angelica Costantini <angelicacostantini07@gmail.com>
2018-08-28 14:14:28 +0200

    more checks + pin code unfinished

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,10 +5,14 @@ class Atm
         @funds = 1000
     end
 
-    def withdraw(amount, account)
+    def withdraw(amount, pin_code, account) 
         case
-        when insufficient_funds_in_account?(amount,account)
+        when insufficient_funds_in_account?(amount, account)
             { status: false, message: 'insufficient funds', date: Date.today }
+        when insufficient_funds_in_atm?(amount)
+            {status: false, message: 'insufficient funds in ATM', date: Date.today}
+        when incorrect_pin?(pin_code, account.pin_code)
+            {status: false, message: 'wrong pin', date: Date.today }
         else
             perform_transaction(amount, account)
         end
@@ -16,9 +20,20 @@ class Atm
 
     private
 
+    def incorrect_pin?(pin_code, actual_pin)
+        pin_code != actual_pin
+    end
+
+    
+
     def insufficient_funds_in_account?(amount, account)
         amount > account.balance
     end
+
+    def insufficient_funds_in_atm?(amount)
+        @funds < amount
+    end
+
     def perform_transaction(amount, account)
         @funds -= amount
         account.balance = account.balance - amount
