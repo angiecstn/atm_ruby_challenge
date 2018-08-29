5eec577b30277371c360d780072d231e01a2cae8
Noel Ryan <noelgerard.ryan@gmail.com>
2018-08-29 17:10:55 +0200

    account owner test pass

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -20,13 +20,13 @@ describe Account do
     expect(subject.account_status).to eq :active
     end
 
-    # it 'is expected to have an owner' do
-    # expect(subject.owner).to eq person
-    # end
+    it 'is expected to have an owner' do
+    expect(subject.owner).to eq person
+    end
 
-    # it 'is expected to raise error if no owner is set' do
-    # expect { described_class.new}.to raise_error 'An Account owner is required'
-    # end
+    it 'is expected to raise error if no owner is set' do
+    expect { described_class.new}.to raise_error 'An Account owner is required'
+    end
 
     it 'deactivates account using Class method' do
         Account.deactivate(subject)
