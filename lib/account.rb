3d2aaa09068e7f8aa9cc30cf7cdb6232a7c38501
Angelica Costantini <angelicacostantini07@gmail.com>
2018-08-29 23:07:21 +0200

    pass test name on initialise + name set

--- lib/account.rb
+++ lib/account.rb
@@ -2,12 +2,13 @@ class Account
     STANDARD_VALIDITY_YRS = 5
     attr_accessor :exp_date
     attr_accessor :account_status
+    attr_accessor :owner 
     
     def initialize(attrs = {})
         @pin_code = rand(1000..9999)
         @account_status = :active
         @exp_date = set_expire_date(exp_date)
-    #     # set_owner(attrs[:owner])
+        set_owner(attrs[:owner])
     end
 
     private
@@ -16,21 +17,17 @@ class Account
         obj == nil ? missing_owner : @owner = obj
     end
 
-    # def missing_owner
-    #     raise "An Account owner is required"
-    # end
-
     def set_expire_date(exp_date)
         Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
 
-    # def activate
-    #     @account_status = :active
-    # end
-
     def self.deactivate(account)
         account.account_status = :deactivated
     end 
+
+    def missing_owner
+        raise "An Account owner is required"
+    end
       
 end
 
